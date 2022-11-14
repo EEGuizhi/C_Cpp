844c676f2adeb3a45aa3022a612d76d2287e2cc4
Guizhi <99305855+EEGuizhi@users.noreply.github.com>
2022-11-14 21:44:32 +0800

    修正Hw1

--- NCHU_EE/HW/4109061012_hw1-2.c
+++ NCHU_EE/HW/4109061012_hw1-2.c
@@ -1,4 +1,4 @@
-// 4109061012 ï¿½ï¿½ï¿½fï¿½ï¿½ 
+// 4109061012 ³¯¬fµ¾
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -6,29 +6,28 @@
 main() {
     int tmp, i, j, num = 0;
     int arr[100];
-    
+
     // generate numbers
     srand((unsigned) time(NULL));
     for(i=0; i<100; i++) {
-    	arr[i] = rand() % 1000;
+    	arr[i] = rand() % 1001;  // Not larger than 1000
 	}
-	
+
 	// bubble sort
 	for(i=0; i<100; i++) {
-    	for(j=0; j<100-j-1; j++) {
+    	for(j=0; j<100-i; j++) {
+			num += 1;
     		if(arr[j+1] < arr[j]) {
-				num += 1;
-
     			tmp = arr[j+1];
     			arr[j+1] = arr[j];
     			arr[j] = tmp;
 			}
 		}
 	}
-	
+
 	// output
 	for(i=0; i<100; i++) {
-		printf("%d ", arr[i]);
+		printf("%3d ", arr[i]);
 	}
-	printf("Number of comparsions: %d", num);
+	printf("\nNumber of comparsions: %d", num);
 }
