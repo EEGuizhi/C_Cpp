16f590d7d3e690c649bf6bb1990bc1aebc148d34
Guizhi <99305855+EEGuizhi@users.noreply.github.com>
2022-11-17 23:24:48 +0800

    update

--- NCHU_EE/迴文判斷.c
+++ NCHU_EE/迴文判斷.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-main() {
+int main() {
     char str[30];
-    int n = 0;
+    int n = 0, i;
     int flag = 1;
     
 	while(1) {
     	scanf("%c", &str[n]);
     	if(str[n] < 48 || str[n] > 57)
     		break;
-    		
+
     	n += 1;
 	}
 	
-	for(int i=0; i<(n/2 + 1); i++) {
+	for(i=0; i<(n/2 + 1); i++) {
 		if(str[i] != str[n-1-i])
 			flag = 0;
 	}
-    
+
     if(flag)
 		printf("YES");
 	else
 		printf("NO");
+
+	return 0;
 }
