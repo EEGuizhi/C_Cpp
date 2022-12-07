db76fa76e0276d3d51d0949950a2a368f2f2cd59
Guizhi <99305855+EEGuizhi@users.noreply.github.com>
2022-12-07 13:44:24 +0800

    update

--- UVA/uva_815.cpp
+++ UVA/uva_815.cpp
@@ -1,22 +1,51 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdio>
+#include <algorithm>
+
+int compare(const void * a, const void * b) {
+	return (*(double*)a - *(double*)b);
+}
 
 int main() {
-	int m, n, elv[30][30], water;
-	int i, j;
+	int m, n, i, j, R = 0;
+	double elv[30 * 30], water, vol;
+	double ans[100][2];
 	
 	while(1) {
-		scanf("%d %d", &m, &n);  // inputs
+		R += 1;
+		// inputs
+		scanf("%d %d", &m, &n);
 		if(m == 0 && n == 0)
 			break;
-		for(i=0; i<m; i++) {
-			for(j=0; j<n; j++) {
-				scanf("%d", &elv[i][j]);
+		n *= m;
+		for(i=0; i<n; i++) {
+			scanf("%lf", &elv[i]);
+		}
+		scanf("%lf", &water);
+		
+		// calc
+		qsort(elv, n, sizeof(double), compare);
+		i = 0;
+		while(water > 0) {
+			i += 1;
+			if(i >= n) {
+				printf("Region %d\n", R);
+				printf("Water level is %.2lf meters.\n", elv[i-1] + (water / (double)(n*100)));
+				printf("100.00 percent of the region is under water.\n");
+				break;
+			}
+			else {
+				vol = (elv[i] - elv[i-1]) * 100 * (double)i;
+				if(water > vol) {
+					water -= vol;
+				}
+				else {  // outputs
+					printf("Region %d\n", R);
+					printf("Water level is %.2lf meters.\n", elv[i-1] + (water / (double)(i*100)));
+					printf("%.2lf percent of the region is under water. i: %d, n: %d\n", (double)100 * i / n, i, n);
+					break;
+				}
 			}
 		}
-		scanf("%d", water);
-		
-		
 	}
 	
 	return 0;
